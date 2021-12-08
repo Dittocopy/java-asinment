ad3d4d02c9dc383bd9a620aec9051314eae14a0a
Dittocopy <71779263+Dittocopy@users.noreply.github.com>
2021-12-08 13:16:01 +0530

    asinmentof_8/12/2021

---
+++ 8-12-2021/ok.java
@@ -0,0 +1,17 @@
+package asinment;
+
+public class ok {
+
+		public static void main(String[] args)
+    	{
+      	  int number = 11;
+        	int NUMBER = 22;
+        	int Number = 33;
+       	 System.out.print(number + " ");
+        	System.out.print(NUMBER + " ");
+        	System.out.println(Number);
+    	
+
+	}
+
+}
